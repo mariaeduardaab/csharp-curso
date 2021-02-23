f0c5b9012f350e377064852b2ae314689e3149c6
Maria Eduarda Alves Bezerra <37427711+mariaeduardaab@users.noreply.github.com>
2021-02-23 19:25:43 -0300

    Exercicio - Calculando volume e circunferencia atraves do raio

--- Program.cs
+++ Program.cs
@@ -1,41 +1,22 @@
 ﻿using System;
 using System.Globalization;
 
-namespace Course
+namespace Course1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Triangulo x, y;
+            Console.WriteLine("Insira o valor do raio: ");
 
-            x = new Triangulo();
-            y = new Triangulo();
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Insira os valores do triangulo X:");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
 
-            Console.WriteLine("Insira os valores do triangulo Y:");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double areaX = x.Area();
-            double areaY = y.Area();
-
-            Console.WriteLine("Area de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Area de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
-            if (areaX > areaY)
-            {
-                Console.WriteLine("Triangulo X possui a maior area!");
-            }
-            else
-            {
-                Console.WriteLine("Triangulo Y possui a maior area!");
-            }
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
 
             Console.ReadKey();
         }
